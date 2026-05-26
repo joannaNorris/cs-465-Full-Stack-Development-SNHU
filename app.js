0172441ea6a5f5cefa2752e2d691c75bd1ad242f
Joanna Norris <gavinswife2023@gmail.com>
2026-05-26 18:36:36 -0500

    updated app.js to include about

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ var logger = require('morgan');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var aboutRouter = require('./app_server/routes/about');
+//FIXME: add other routers here
 var handlebars = require('hbs');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server/views'));
 
 //register handlebars partials
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.set('view engine', 'hbs');
+app.set('layout', 'layouts/layout');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
