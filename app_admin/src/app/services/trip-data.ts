5447717c5bdebf368b3f6d27b0400a6738da1a6a
Joanna Norris <gavinswife2023@gmail.com>
2026-06-16 23:10:27 -0500

    trip data service introduced to trip model

--- app_admin/src/app/services/trip-data.ts
+++ app_admin/src/app/services/trip-data.ts
@@ -1,4 +1,22 @@
-import { Service } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Service()
-export class TripData {}
+import { Trip } from '../models/trip';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class TripData { 
+    constructor(private http: HttpClient) { }
+
+    getTrips(): Observable<Trip[]> {
+        let url = '/api/trips';
+        return this.http.get<Trip[]>(url);
+    }
+
+    getTripById(id: string): Observable<Trip> {
+        return this.http.get<Trip>(`/api/trips/${id}`);
+    }
+}
