a5dcf0a67a1d3844bc92db0187aac9695e23b675
Joanna Norris <gavinswife2023@gmail.com>
2026-06-26 17:09:40 -0500

    index.js route updated with register and login routes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const tripsController = require('../controllers/trips');
+const authController = require('../controllers/authentication');
+
+router.route("/register").post(authController.register); //POST route for user registration
+router.route("/login").post(authController.login); //POST route for user login
 
 //define route for trips endpoint
 router
     .route('/trips')
     .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
+    .post(authenticateJWT, tripsController.tripsAddTrip);
 
 //GET route for tripsFindByCode
 router
     .route('/trips/:code')
     .get(tripsController.tripsFindByCode)
-    .put(tripsController.tripsUpdateTrip);
+    .put(authenticateJWT, tripsController.tripsUpdateTrip);
 
 module.exports = router;
