39319d70a26d7c101034ea8892e380a3eedf7c82
Joanna Norris <gavinswife2023@gmail.com>
2026-06-27 17:23:24 -0500

    navbar html updated to no longer be static

--- app_admin/src/app/app.ts
+++ app_admin/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
 //import { TripListing } from './trip-listing/trip-listing';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
