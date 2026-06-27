ed092d129d295853f7c28c74e9335b8fe73801fc
Joanna Norris <gavinswife2023@gmail.com>
2026-06-26 20:57:16 -0500

    login route added to router

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,7 +4,7 @@ const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
 
 router.route("/register").post(authController.register); //POST route for user registration
-//router.route("/login").post(authController.login); //POST route for user login
+router.route("/login").post(authController.login); //POST route for user login
 
 //define route for trips endpoint
 router
