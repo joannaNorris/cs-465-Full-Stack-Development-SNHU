5cc1f114f3d6240a1477127b8ed3451f8176ea1c
Joanna Norris <gavinswife2023@gmail.com>
2026-06-26 23:10:31 -0500

    storage class code updated

--- app_admin/src/app/storage.ts
+++ app_admin/src/app/storage.ts
@@ -1,1 +1,11 @@
-export class Storage {}
+import { InjectionToken } from '@angular/core';
+
+export const BROWSER_STORAGE = new
+    InjectionToken<Storage>('Browser Storage', {
+    providedIn: 'root',
+    factory: () => localStorage
+    });
+
+export class Storage {
+    
+}
